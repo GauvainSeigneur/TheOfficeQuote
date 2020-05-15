84570bea905e30a645b8edcbb216602caed303d1
Gauvain <seigneur.gauvain@gmail.com>
2020-05-15 15:14:20 +0200

    step 6: set primary for token entity

--- data-adapter/src/main/java/com/gauvain/seigneur/data_adapter/model/TokenModel.kt
+++ data-adapter/src/main/java/com/gauvain/seigneur/data_adapter/model/TokenModel.kt
@@ -1,5 +1,11 @@
 package com.gauvain.seigneur.data_adapter.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class TokenModel(
+    @PrimaryKey
+    val id:Int= 0,
     val token: String
 )
