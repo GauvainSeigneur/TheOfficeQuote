86405c96982cbb1b74020891314dc60906c79ad9
Gauvain <seigneur.gauvain@gmail.com>
2020-05-18 14:01:47 +0200

    step 32: fix random quote route after website down

--- data-adapter/src/main/java/com/gauvain/seigneur/data_adapter/service/FavQuoteService.kt
+++ data-adapter/src/main/java/com/gauvain/seigneur/data_adapter/service/FavQuoteService.kt
@@ -17,7 +17,7 @@ interface FavQuoteService {
     @GET("users/{login}")
     fun getUser(@Path(value = "login", encoded = true) login: String): Call<CurrentUser>
 
-    @GET("quotes")
+    @GET("qotd")
     fun getRandom(): Call<SingleQuote>
 
 }
